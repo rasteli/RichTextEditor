6103249be736881b33a998310b85ad1f88733408
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2021-08-13 21:14:19 -0300

    Removed doubled import statement

--- src/App.js
+++ src/App.js
@@ -1,64 +1,60 @@
-import "./App.css";
-import React, { useCallback, useMemo, useState } from "react";
-import { Editable, Slate, withReact } from "slate-react";
-import { createEditor } from "slate";
+import React, { useCallback, useMemo, useState } from "react"
+import { Editable, Slate, withReact } from "slate-react"
+import { createEditor } from "slate"
 import {
-    Toolbar,
-    LeafElement,
-    CodeElement,
-    DefaultElement,
-    LinkElement,
-    TitleElement,
-    QuoteElement,
-} from "./components";
+  Toolbar,
+  LeafElement,
+  CodeElement,
+  DefaultElement,
+  LinkElement,
+  TitleElement,
+  QuoteElement
+} from "./components"
 
-import "./App.css";
+import "./App.css"
 
 function App() {
-    const editor = useMemo(() => withReact(createEditor()), []);
-    const [value, setValue] = useState([
-        {
-            type: "paragraph",
-            children: [{ text: "Hello, world" }],
-        },
-    ]);
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "Hello, world" }]
+    }
+  ])
 
-    const renderElement = useCallback((props) => {
-        switch (props.element.type) {
-            case "H1":
-                return <TitleElement {...props} />;
-            case "code":
-                return <CodeElement {...props} />;
-            case "link":
-                return <LinkElement {...props} />;
-            case "quote":
-                return <QuoteElement {...props} />;
-            default:
-                return <DefaultElement {...props} />;
-        }
-    }, []);
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case "H1":
+        return <TitleElement {...props} />
+      case "code":
+        return <CodeElement {...props} />
+      case "link":
+        return <LinkElement {...props} />
+      case "quote":
+        return <QuoteElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
 
-    const renderLeaf = useCallback((props) => {
-        return <LeafElement {...props} />;
-    }, []);
+  const renderLeaf = useCallback(props => {
+    return <LeafElement {...props} />
+  }, [])
 
-    return (
-        <Slate
-            editor={editor}
-            value={value}
-            onChange={(newValue) => setValue(newValue)}
-        >
-            <body id="body">
-                <div id="editor">
-                    <Toolbar editor={editor} />
-                    <Editable
-                        renderElement={renderElement}
-                        renderLeaf={renderLeaf}
-                    />
-                </div>
-            </body>
-        </Slate>
-    );
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={newValue => setValue(newValue)}
+    >
+      <body id="body">
+        <div id="editor">
+          <Toolbar editor={editor} />
+          <Editable renderElement={renderElement} renderLeaf={renderLeaf} />
+        </div>
+      </body>
+    </Slate>
+  )
 }
 
-export default App;
+export default App
--- src/components/CustomEditor.js
+++ src/components/CustomEditor.js
@@ -1,159 +1,155 @@
-import { Editor, Text, Transforms } from "slate";
+import { Editor, Text, Transforms } from "slate"
 
-const markOptions = { match: (n) => Text.isText(n), split: true };
+const markOptions = { match: n => Text.isText(n), split: true }
 
-const blockOptions = (editor) => {
-    return { match: (n) => Editor.isBlock(editor, n) };
-};
+const blockOptions = editor => {
+  return { match: n => Editor.isBlock(editor, n) }
+}
 
 const getActiveness = (isActive, secondValue = true) => {
-    return isActive ? null : secondValue;
-};
+  return isActive ? null : secondValue
+}
 
 const CustomEditor = {
-    isBoldMark(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.bold === true,
-            universal: true,
-        });
+  isBoldMark(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.bold === true,
+      universal: true
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isItalicMark(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.italic === true,
-            universal: true,
-        });
+  isItalicMark(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.italic === true,
+      universal: true
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isUnderlineMark(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.underline === true,
-            universal: true,
-        });
+  isUnderlineMark(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.underline === true,
+      universal: true
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isCodeBlock(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.type === "code",
-        });
+  isCodeBlock(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === "code"
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isLinkBlock(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.type === "link",
-        });
+  isLinkBlock(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === "link"
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isTitleBlock(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.type === "H1",
-        });
+  isTitleBlock(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === "H1"
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    isQuoteBlock(editor) {
-        const [match] = Editor.nodes(editor, {
-            match: (n) => n.type === "quote",
-        });
+  isQuoteBlock(editor) {
+    const [match] = Editor.nodes(editor, {
+      match: n => n.type === "quote"
+    })
 
-        return !!match;
-    },
+    return !!match
+  },
 
-    toggleBoldMark(editor) {
-        const isActive = this.isBoldMark(editor);
+  toggleBoldMark(editor) {
+    const isActive = this.isBoldMark(editor)
 
-        Transforms.setNodes(
-            editor,
-            { bold: getActiveness(isActive) },
-            markOptions
-        );
-    },
+    Transforms.setNodes(editor, { bold: getActiveness(isActive) }, markOptions)
+  },
 
-    toggleItalicMark(editor) {
-        const isActive = this.isItalicMark(editor);
+  toggleItalicMark(editor) {
+    const isActive = this.isItalicMark(editor)
 
-        Transforms.setNodes(
-            editor,
-            { italic: getActiveness(isActive) },
-            markOptions
-        );
-    },
+    Transforms.setNodes(
+      editor,
+      { italic: getActiveness(isActive) },
+      markOptions
+    )
+  },
 
-    toggleUnderlineMark(editor) {
-        const isActive = this.isUnderlineMark(editor);
+  toggleUnderlineMark(editor) {
+    const isActive = this.isUnderlineMark(editor)
 
-        Transforms.setNodes(
-            editor,
-            { underline: getActiveness(isActive) },
-            markOptions
-        );
-    },
+    Transforms.setNodes(
+      editor,
+      { underline: getActiveness(isActive) },
+      markOptions
+    )
+  },
 
-    toggleCodeBlock(editor) {
-        const isActive = this.isCodeBlock(editor);
+  toggleCodeBlock(editor) {
+    const isActive = this.isCodeBlock(editor)
 
-        Transforms.setNodes(
-            editor,
-            { type: getActiveness(isActive, "code") },
-            blockOptions(editor)
-        );
-    },
+    Transforms.setNodes(
+      editor,
+      { type: getActiveness(isActive, "code") },
+      blockOptions(editor)
+    )
+  },
 
-    toggleTitleBlock(editor) {
-        const isActive = this.isTitleBlock(editor);
+  toggleTitleBlock(editor) {
+    const isActive = this.isTitleBlock(editor)
 
-        Transforms.setNodes(
-            editor,
-            { type: getActiveness(isActive, "H1") },
-            blockOptions(editor)
-        );
-    },
+    Transforms.setNodes(
+      editor,
+      { type: getActiveness(isActive, "H1") },
+      blockOptions(editor)
+    )
+  },
 
-    toggleQuoteBlock(editor) {
-        const isActive = this.isQuoteBlock(editor);
+  toggleQuoteBlock(editor) {
+    const isActive = this.isQuoteBlock(editor)
 
-        Transforms.setNodes(
-            editor,
-            { type: getActiveness(isActive, "quote") },
-            blockOptions(editor)
-        );
-    },
+    Transforms.setNodes(
+      editor,
+      { type: getActiveness(isActive, "quote") },
+      blockOptions(editor)
+    )
+  },
 
-    toggleLinkBlock(editor) {
-        const isActive = this.isLinkBlock(editor);
-        let url = "";
-        let type = "";
+  toggleLinkBlock(editor) {
+    const isActive = this.isLinkBlock(editor)
+    let url = ""
+    let type = ""
 
-        if (!isActive) {
-            url = window.prompt("URL");
+    if (!isActive) {
+      url = window.prompt("URL")
 
-            if (url) {
-                if (url.substring(0, 8) !== "https://") {
-                    url = `https://${url}`;
-                    console.log(url.substring(8, url.length));
-                }
-
-                type = "link";
-            } else {
-                type = null;
-            }
-        } else {
-            type = null;
+      if (url) {
+        if (url.substring(0, 8) !== "https://") {
+          url = `https://${url}`
+          console.log(url.substring(8, url.length))
         }
 
-        Transforms.setNodes(editor, { type, href: url }, blockOptions(editor));
-    },
-};
+        type = "link"
+      } else {
+        type = null
+      }
+    } else {
+      type = null
+    }
 
-export default CustomEditor;
+    Transforms.setNodes(editor, { type, href: url }, blockOptions(editor))
+  }
+}
+
+export default CustomEditor
